0a1e6c2a9a45f1e6968b57a8879ae283ec0f71be
Rafael Pérez García <rafael.perez@imperdiblesoft.com>
2022-01-22 14:36:00 +0100

    Bugfix some types

--- src/apiClient/index.ts
+++ src/apiClient/index.ts
@@ -1,21 +1,21 @@
 import { Currency } from "../types";
 import { BitcoinHistoricalFromServer, BitcoinValueFromServer } from "./types";
 
-const getBitcoinPrice = async ({ key }: Currency): Promise<BitcoinValueFromServer> => {
+const getBitcoinPrice = async ({ key }: Currency): Promise<BitcoinValueFromServer | void> => {
   try {
     const response = await fetch(`https://api.coindesk.com/v1/bpi/currentprice/${ key }.json`);
     const value = await response.json();
-    return value;
+    return value as BitcoinValueFromServer;
   } catch (err) {
     console.error(err);
   }
 }
 
-const getHistoricalData = async ({ key }: Currency): Promise<BitcoinHistoricalFromServer> => {
+const getHistoricalData = async ({ key }: Currency): Promise<BitcoinHistoricalFromServer | void> => {
   try {
     const response = await fetch(`https://api.coindesk.com/v1/bpi/historical/close.json?currency=${ key }`);
     const historical = await response.json();
-    return historical;
+    return historical as BitcoinHistoricalFromServer;
   } catch (err) {
     console.error(err);
   }
