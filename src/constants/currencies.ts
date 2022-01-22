ac855776adf87062e24e25dc4307358499bd6ddf
Rafael Pérez García <rafael.perez@imperdiblesoft.com>
2022-01-22 14:14:28 +0100

    Add selectable currencies

---
+++ src/constants/currencies.ts
@@ -0,0 +1,24 @@
+import { Currency } from "../types";
+
+export const currencies: Currency[] = [
+  {
+    key: 'USD',
+    name: 'U.S.A. Dollar',
+  },
+  {
+    key: 'EUR',
+    name: 'Euro',
+  },
+  {
+    key: 'CNY',
+    name: 'Yuan',
+  },
+  {
+    key: 'JPY',
+    name: 'yen',
+  },
+  {
+    key: 'PLN',
+    name: 'Esloti',
+  },
+]
